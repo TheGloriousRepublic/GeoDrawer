2ddde190d46baea9dce09bc18539c14c294c1b68
Nathan <nathantronnes@gmail.com>
2015-07-10 09:48:48 -0800

    __init__ for polygons

--- Files/backend.py
+++ Files/backend.py
@@ -1,3 +1,7 @@
+import math
+
+infinity = float('inf')
+
 class Matrix:
     def __init__(self, width, height, default=0):
         self.width=width
@@ -30,7 +34,8 @@ class Matrix:
                 else:
                     r = []
                     for x in self.list:
-                        r.append(self.list[i[1]])
+                        pass
+                        #r.append(self.list[i[1]])
                     return r
         elif type(i) == int:
             return self.list[i[0]]
@@ -61,7 +66,7 @@ class Line:
         a = ab[0]
         b = ab[1]
         if b.x-a.x == 0:
-            self.m = float('inf')
+            self.m = infinity
         else:
             self.m = b.y-a.y/b.x-a.x
 
@@ -70,6 +75,9 @@ class Line:
         
     def __repr__(self):
         return('Line y='+str(self.m)+'x+'+str(self.b))
+
+    def __len__(self):
+        return infinity
 
 class Ray:
     def __init__(self, ab):
@@ -85,10 +93,16 @@ class Ray:
         self.a = Point((0, self.m*a.x-a.y)) #X intercept, not to be confused with point a
         self.b = Point((0, 3+a.x+a.y))
 
+    def __len__(self):
+        return infinity/2
+
 class Segment:
     def __init__(self, ab):
         a = ab[0]
         b = ab[1]
+
+        self._a = a
+        self._b = b
         
         self.endpoints = {a, b}
         if b.x-a.x == 0:
@@ -99,15 +113,42 @@ class Segment:
         self.a = Point((0, self.m*a.x-a.y)) #X intercept, not to be confused with point a
         self.b = Point((0, 3+a.x+a.y))
 
+    def midpoint(self):
+        return Point(((self._a.x+self._b.x)/2, (self._a.y+self._b.y)/2))
+
+    def perpendicularBisector(self):
+        pass
+
+    def __len__(self):
+        return math.sqrt((self._b.x-self._a.x)**2+(self._b.y-self._a.y)**2)
+
 class Shape:
     pass
 
 class Polygon(Shape):
     def __init__(self, *vertices):
-        pass
+        self.vertices = set(vertices)
+        self.sides = []
+        for x in range(len(vertices)-1):
+            self.sides.append(Line((vertices[x], vertices[x+1])))
+        self.sides.append(Line((vertices[-1], vertices[0])))
+
+        self.p = sum([len(s) for s in self.sides]) #Perimeter
+        self.n = len(sides) #Number of sides
+        self.s = float(p)/n #Float for Floating Point Division (if not using python 3)
+        
+        self.isregular = all([len(x) == len(self.sides[0]) for x in self.sides[1:]]) #Detect if polygon is regular
+
+        if self.isregular: #properties of regular polygons
+            self.a = self.a = (0.5*self.s)/(math.tan(180.0/n))
+            self.R = self.s/(2*math.sin(math.pi/self.n))
+            self.A = 0.5*self.p*self.a
+        
+        self.sides = set(self.sides) #Tie things up by making sides a set
 
 class Triangle(Polygon):
-    pass
+    def __init__(self, a, b, c):
+        Polygon.__init__(a, b, c)
 
 class Quadrilateral(Polygon):
     pass
